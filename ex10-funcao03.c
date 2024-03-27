92852c92c1e6ccb1960de75b13d06d7969c24deb
cpmart <cpmart@gmail.com>
2024-03-26 21:49:08 -0300

    3 menus com função

--- ex10-funcao03.c
+++ ex10-funcao03.c
@@ -1,41 +1,38 @@
 #include <stdio.h>
 
-//Protótipo
+// Protótipo
 int menuPrincipal(void);
+int menuArea(void);
+int menuVolume(void);
+float areaQuadrado(float lado);
+float areaCirculo(float raio);
+float volumeCubo(float lado);
+float volumeCilindro(float raio, float altura);
+int escolherMenu();
 int main()
 {
     int opcao, opcaoSubMenu;
     float lado, raio, altura;
     while (opcao != 0)
     {
-        printf("========MENU PRINCIPAL============\n");
-        printf("[1] Área\n");
-        printf("[2] Volume\n");
-        printf("[0] Sair\n");
-        printf("Escolha uma opção: ");
-        scanf("%d", &opcao);
+        opcao = menuPrincipal();
         switch (opcao)
         {
         case 1:
             do
             {
-                printf("========MENU ÁREA============\n");
-                printf("[1] Área Quadrado\n");
-                printf("[2] Área Círculo\n");
-                printf("[0] Sair\n");
-                printf("Escolha uma opção: ");
-                scanf("%d", &opcaoSubMenu);
+                opcaoSubMenu = menuArea();
                 switch (opcaoSubMenu)
                 {
                 case 1:
                     printf("Lado: ");
                     scanf("%f", &lado);
-                    printf("Área do quadrado: %g\n", lado * lado);
+                    printf("Área do quadrado: %g\n", areaQuadrado(lado));
                     break;
                 case 2:
                     printf("Raio: ");
                     scanf("%f", &raio);
-                    printf("Área do círculo: %g\n", 3.14 * raio * raio);
+                    printf("Área do círculo: %g\n", areaCirculo(raio));
                     break;
                 case 0:
                     break;
@@ -48,25 +45,20 @@ int main()
         case 2:
             do
             {
-                printf("========MENU VOLUME============\n");
-                printf("[1] Volume do Cubo\n");
-                printf("[2] Volume do Cilindro\n");
-                printf("[0] Sair\n");
-                printf("Escolha uma opção: ");
-                scanf("%d", &opcaoSubMenu);
+                opcaoSubMenu = menuVolume();
                 switch (opcaoSubMenu)
                 {
                 case 1:
                     printf("Lado: ");
                     scanf("%f", &lado);
-                    printf("Volume do cubo: %g\n", lado * lado * lado);
+                    printf("Volume do cubo: %g\n", volumeCubo(lado));
                     break;
                 case 2:
                     printf("Raio: ");
                     scanf("%f", &raio);
                     printf("Altura: ");
                     scanf("%f", &altura);
-                    printf("Volume do cilindro: %g\n", 3.14 * raio * raio * altura);
+                    printf("Volume do cilindro: %g\n", volumeCilindro(raio, altura));
                     break;
                 case 0:
                     break;
@@ -84,4 +76,53 @@ int main()
             break;
         }
     }
+}
+int menuPrincipal(void)
+{
+    printf("========MENU PRINCIPAL============\n");
+    printf("[1] Área\n");
+    printf("[2] Volume\n");
+    printf("[0] Sair\n");
+    return escolherMenu();
+}
+
+int menuArea()
+{
+    printf("========MENU ÁREA============\n");
+    printf("[1] Área Quadrado\n");
+    printf("[2] Área Círculo\n");
+    printf("[0] Sair\n");
+    return escolherMenu();
+}
+
+int menuVolume()
+{
+    printf("========MENU VOLUME============\n");
+    printf("[1] Volume do Cubo\n");
+    printf("[2] Volume do Cilindro\n");
+    printf("[0] Sair\n");
+    return escolherMenu();
+}
+
+float areaQuadrado(float lado){
+    return lado*lado;
+}
+
+float areaCirculo(float raio){
+    return 3.14 * raio * raio;
+}
+
+float volumeCubo(float lado){
+    return lado * areaQuadrado(lado);
+}
+
+float volumeCilindro(float raio, float altura){
+    return altura * areaCirculo(raio);
+}
+
+int escolherMenu(){
+    int opcao;
+    printf("Escolha uma opção: ");
+    scanf("%d", &opcao);
+    return opcao;
 }
